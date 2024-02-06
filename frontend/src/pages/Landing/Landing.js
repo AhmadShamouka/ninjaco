5f9bde3d6220c0a09762ef97687ea83e6d162751
AhmadShamouka <shamoukaahmad@gmail.com>
2024-02-06 15:05:27 +0200

    frontend: Created a folder for button component

---
+++ frontend/src/pages/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Landing.css";
+const Landing = () => {
+  return (
+    <div>
+      <section className="hero-container">
+        <div className="hero-img">
+          <h1>Learn coding and robotics with fun and easy code blocks.</h1>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Landing;
